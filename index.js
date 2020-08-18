0fb08b0c7c2c31d993e9cd8bffd80675e3cada14
Alexander Danowicz <alexdanowicz@gmail.com>
2020-08-18 09:22:32 -0300

    refactoring the copy url function

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-let openBtn;
 const settings = {
 
     parameters: {
@@ -6,26 +5,45 @@ const settings = {
         shadowOpacity: '.8', // from 0 to 1
         fontColor: '#444',
         animations: true,
-        links: [ 'copy', 'email', 'whatsapp', 'facebook', 'linkedin', 'telegram', 'twitter' ],
+        links: [ 'copy', 'email', 'whatsapp', 'linkedin', 'telegram', 'twitter' ],
         message: 'Da só uma olhada nesse link!'
     },
 
-    linkParameters(links) {
-    
-        links.forEach(link, () => {
-            return 
-            [
-                {
-                    name: copy,
-                    icon: 'fas fa-copy',
-                    copyURL() {
-                        
-                    }
+    linkParameters(message, url) {
+        let shareData = [];
+        message = encodeURIComponent(message);
+        this.parameters.links.forEach(link => {
+            let icon = `fab fa-${link}`;
+            let shareLink;
+            switch (link) {
+                case 'copy':
+                    icon = 'fas fa-copy'
+                    break;
+                case 'email':
+                    icon = 'fas fa-envelope'
+                    shareLink =`mailto:?subject=${message}&amp;body=${url}`
+                    break;
+                case 'whatsapp':
+                    shareLink = `https://wa.me/?text=${message}+${url}`
+                    break;
+                case 'facebook':
+                    shareLink = `https://www.facebook.com/sharer/sharer.php?u=${url}`
+                    break;
+                case 'twitter':
+                    shareLink = `https://twitter.com/intent/tweet?text=${message}+${url}`    
+                    break;
+                case 'linkedin':
+                    shareLink = `https://www.linkedin.com/shareArticle?mini=true&url=${url}`
+                    break;
+                case 'telegram':
+                    shareLink = `https://t.me/share/url?url=${url}&text=${message}`
+                    break;
+            }
 
-                }
-            ]
+            if (shareLink) shareData.push({name: link, icon, shareLink});
+            else shareData.push({name: link, icon});
         })
-    
+        return shareData;
     },
 
     config({bgColor, shadowOpacity, fontColor, animations}) {
@@ -42,91 +60,50 @@ const settings = {
     }
 }
 
+const renders = {
+    buttonRender(params) {
+        const buttonsGrid = document.querySelector('.buttons__grid');
+        params.forEach((link => {
+            buttonsGrid.innerHTML +=`
+                <div class="buttons__wrapper">
+                    <a target="_blank" ${link.shareLink ? `href=${link.shareLink}` : "id=copy"} class="buttons__btn">
+                        <i class="${link.icon}"></i>
+                    </a>
+                    <p class="buttons__label">${link.name}</p>
+                </div>`
+        }))
+    },
 
-}
+    panelRender() {
+        const panelDiv = `
 
-const buttonRender = ({links, message}) => {
-
-    const buttonGrid = document.querySelector('.buttons__grid');
-    const url = window.location.href;
-    const text = encodeURIComponent(message);
-
-    links.forEach(link => {
-        let icon = `fab fa-${link}`
-        let shareLink;
-
-        switch (link) {
-            case 'copy':
-                icon = "fas fa-copy"
-                link = "Copiar URL"
-                break;
-            case 'email':
-                icon = "fas fa-envelope"
-                shareLink = `mailto:?subject=${text}&amp;body=${url}`
-                break;
-            case 'whatsapp':
-                shareLink = `https://wa.me/?text=${text}+${url}`
-                break;
-            case 'facebook':
-                shareLink = `https://www.facebook.com/sharer/sharer.php?u=${url}`
-                break;
-            case 'twitter':
-                shareLink = `https://twitter.com/intent/tweet?text=${text}+${url}`
-                break;
-            case 'linkedin':
-                shareLink = `https://www.linkedin.com/shareArticle?mini=true&url=${url}`
-                break;
-            case 'telegram':
-                shareLink = `https://t.me/share/url?url=${url}&text=${text}`
-                break;
-        }
-        
-        buttonGrid.innerHTML +=
-           `<div class="buttons__wrapper">
-                <a target="_blank" ${shareLink ? `href=${shareLink}` : "id=copy"} class="buttons__btn">
-                    <i class="${icon}"></i>
-                </a>
-                <p class="buttons__label">${link}</p>
+            <div id="panel" class="panel">
+                <button id="btn-close" class="panel__close">✕</button>
+                <p class="panel__title">Compartilhar</p>
+                <div class="buttons__grid"></div>
             </div>`
-    });
 
-    // Create an input -> set input value to URL -> copy input value -> remove input;
-    const copyBtn = document.getElementById('copy');
-    copyBtn.addEventListener('click', () => {
-        
-    })
+        const shadowDiv = '<div id="shadow"></div>'
+    
+        document.body.innerHTML += panelDiv + shadowDiv;
+    
+        const panel = document.getElementById('panel');
+        const shadow = document.getElementById('shadow');
+    
+        settings.config(settings.parameters);
+    
+        this.buttonRender(settings.linkParameters(settings.parameters.message, window.location.href));
+    
+        const closeBtn = document.getElementById('btn-close');
+    
+        closeBtn.addEventListener('click', () => {
+            panel.remove();
+            shadow.remove();
+        });
+    }
 }
 
-const panelRender = () => {
-    const panelDiv = '<div id="panel" class="panel"></div>'
-    const shadowDiv = '<div id="shadow"></div>'
-
-    document.body.innerHTML += panelDiv + shadowDiv;
-
-    const panel = document.getElementById('panel');
-    const shadow = document.getElementById('shadow');
-
-    settings.config(settings.parameters);
-
-    panel.innerHTML =
-    `
-        <button id="btn-close" class="panel__close">✕</button>
-        <p class="panel__title">Compartilhar</p>
-        <div class="buttons__grid"></div>
-    `
-
-    buttonRender(settings.parameters); 
-
-    const closeBtn = document.getElementById('btn-close');
-
-    closeBtn.addEventListener('click', () => {
-        panel.remove();
-        shadow.remove();
-    });
-
-};
-
-openBtn = document.getElementById('btn-open');
+const openBtn = document.getElementById('btn-open');
 openBtn.addEventListener('click', () => {
-    panelRender();
+    renders.panelRender();
 });
